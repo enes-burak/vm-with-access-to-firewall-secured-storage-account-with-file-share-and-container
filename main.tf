e2d3a4e495872826530663aea5ae4c40c48e7500
Burak Kahraman <burakistan96@hotmail.com>
2023-06-18 06:23:58 -0400

    na3

---
+++ main.tf
@@ -0,0 +1,157 @@
+#########   TASK 1    ###########################################
+
+# Create first resource group
+
+resource "azurerm_resource_group" "rg1"{
+  name     = var.rg_name
+  location = var.location
+}
+
+# Create a virtual network inside of our resource group
+
+resource "azurerm_virtual_network" "vnet"{
+    name = var.vnet_name
+    address_space = var.vnet_address_space
+    location = azurerm_resource_group.rg1.location
+    resource_group_name = azurerm_resource_group.rg1.name
+}
+
+# Create a subnet within our virtual network
+
+resource "azurerm_subnet" "subnet"{
+    name = var.subnet_name
+    resource_group_name = azurerm_resource_group.rg1.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes = var.subnet_address_space
+}
+
+# Create a network interface card for our VM to use
+
+resource "azurerm_network_interface" "nci1" {
+  name                = var.nci1_name
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  ip_configuration {
+    name                          = var.ipconfig1_name
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = var.ipconfig1_alloc
+  }
+}
+
+# Create a Linux VM and connect it to our Network Interface Card
+
+resource "azurerm_virtual_machine" "vm1"{
+    name = var.vm1_name
+    resource_group_name = azurerm_resource_group.rg1.name
+    location = azurerm_resource_group.rg1.location
+    network_interface_ids = [azurerm_network_interface.nci1.id]
+    vm_size = var.vm1_size
+    storage_image_reference {
+    publisher = var.storage_image_ref_pub
+    offer     = var.storage_image_ref_offer
+    sku       = var.storage_image_ref_sku
+    version   = var.storage_image_ref_version
+    }
+    storage_os_disk {
+    name              = var.storage_os_disk1_name
+    caching           = var.storage_os_disk1_caching
+    create_option     = var.storage_os_disk1_create_option
+    managed_disk_type = var.storage_os_disk1_managed_disk_type
+    }
+    os_profile {
+    computer_name  = var.os_profile_comp_name
+    admin_username = var.os_profile_user
+    admin_password = var.os_profile_pass
+    }
+    os_profile_linux_config {
+    disable_password_authentication = var.os_profile_linux_config1
+    }
+}
+
+
+############# TASK 2    #####################################
+
+# Create second resource group along with a storage account inside it
+
+resource "azurerm_resource_group" "rg2"{
+  name     = var.rg_name2
+  location = var.location
+}
+resource "azurerm_storage_account" "storage_acc1" {
+  name                     = var.storage_acc1_name
+  resource_group_name      = azurerm_resource_group.rg2.name
+  location                 = azurerm_resource_group.rg2.location
+  account_tier             = var.storage_acc1_acc_tier
+  account_replication_type = var.storage_acc1_replic_type
+}
+
+
+############### Task 3 #########################################
+
+# Create container
+
+resource "azurerm_storage_container" "container1" {
+  name                  = var.container1_name
+  storage_account_name  = azurerm_storage_account.storage_acc1.name
+  container_access_type = var.container1_access_type
+}
+
+# Uploading of "LICENSE" file done through Azure Portal
+
+################ Task 4 ########################################
+
+
+################ Task 5 ########################################
+
+# Create file share 
+
+
+resource "azurerm_storage_share" "file_share1" {
+  name                 = var.f_share_name
+  storage_account_name = azurerm_storage_account.storage_acc1.name
+  quota                = var.f_share_quota
+
+  acl {
+    id = var.f_share_acl_id
+    access_policy {
+        permissions = var.f_share_access_policy_permissions
+        start       = var.f_share_access_policy_start
+        expiry      = var.f_share_access_policy_expiry
+    }
+  }
+}
+
+################# Task 6 ########################################
+
+# Creating a firewall to limit public access
+
+# resource "azurerm_subnet" "firewallsubnet" {
+#   name                 = var.firewall_subnet_name
+#   resource_group_name  = azurerm_resource_group.rg1.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefixes     = var.firewall_subnet_address_space
+# }
+
+# resource "azurerm_public_ip" "publicip1" {
+#   name                = var.public_ip_name
+#   location            = azurerm_resource_group.rg1.location
+#   resource_group_name = azurerm_resource_group.rg1.name
+#   allocation_method   = var.public_ip_allocation
+#   sku                 = var.public_ip_sku
+# }
+
+# resource "azurerm_firewall" "firewall1" {
+#   name                = var.firewall_name
+#   location            = azurerm_resource_group.rg1.location
+#   resource_group_name = azurerm_resource_group.rg1.name
+#   sku_name            = var.firewall_sku_name
+#   sku_tier            = var.firewall_sku_tier
+
+#   ip_configuration {
+#     name                 = var.ip_config_name
+#     subnet_id            = azurerm_subnet.firewallsubnet.id
+#     public_ip_address_id = azurerm_public_ip.publicip1.id
+#   }
+# }
+
